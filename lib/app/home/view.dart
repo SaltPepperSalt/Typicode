a7c6888015f596001de7890b1b1ba5f77581dee5
SaltPepperSalt <dksro324@gmail.com>
2023-10-07 15:18:05 +0900

    FEAT: apply double back terminate

--- lib/app/home/view.dart
+++ lib/app/home/view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:typicode/app/core/utils/double_back_terminate.dart';
 import 'package:typicode/app/home/logic.dart';
 import 'package:typicode/routes/routes.dart';
 
@@ -9,43 +10,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('${logic.authService.user?.name} Home'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              logic.authService.logout();
-              Get.offAllNamed(Routes.login);
-            },
-            child: const Text('logout'),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Obx(
-          () => logic.isLoadingPostForLists.value
-              ? const CircularProgressIndicator()
-              : ListView.separated(
-                  itemBuilder: (context, index) => ListTile(
-                    onTap: () {
-                      Get.toNamed(
-                        '${Routes.postDetail}/${logic.postForLists[index].value.id.toString()}',
-                      );
-                    },
-                    title: Text('${logic.postForLists[index].value.title}'),
-                    trailing: Obx(
-                      () => Text(
-                          'comments : ${logic.postForLists[index].value.commentCount}'),
+    return DoubleBackTerminate(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('${logic.authService.user?.name} Home'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                logic.authService.logout();
+                Get.offAllNamed(Routes.login);
+              },
+              child: const Text('logout'),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Obx(
+            () => logic.isLoadingPostForLists.value
+                ? const CircularProgressIndicator()
+                : ListView.separated(
+                    itemBuilder: (context, index) => ListTile(
+                      onTap: () {
+                        Get.toNamed(
+                          '${Routes.postDetail}/${logic.postForLists[index].value.id.toString()}',
+                        );
+                      },
+                      title: Text('${logic.postForLists[index].value.title}'),
+                      trailing: Obx(
+                        () => Text(
+                            'comments : ${logic.postForLists[index].value.commentCount}'),
+                      ),
                     ),
+                    separatorBuilder: (context, index) => const Divider(
+                      height: 1,
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    itemCount: logic.postForLists.length,
                   ),
-                  separatorBuilder: (context, index) => const Divider(
-                    height: 1,
-                    thickness: 1,
-                    color: Colors.grey,
-                  ),
-                  itemCount: logic.postForLists.length,
-                ),
+          ),
         ),
       ),
     );
