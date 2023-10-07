e7e49547806b1936a7b4ca4a0584e51620130478
SaltPepperSalt <dksro324@gmail.com>
2023-10-07 14:02:35 +0900

    FEAT: show post list on hone page

--- lib/app/home/view.dart
+++ lib/app/home/view.dart
@@ -12,17 +12,32 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('${logic.authService.user?.name} Home'),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: TextButton(
+        actions: [
+          TextButton(
             onPressed: () {
               logic.authService.logout();
               Get.offAllNamed(Routes.login);
             },
             child: const Text('logout'),
           ),
-        ),
+        ],
+      ),
+      body: Center(
+        child: logic.isLoadingPostForLists.value
+            ? const CircularProgressIndicator()
+            : ListView.separated(
+                itemBuilder: (context, index) => ListTile(
+                  title: Text('${logic.postForLists[index].value.title}'),
+                  trailing: Text(
+                      'comments : ${logic.postForLists[index].value.commentCount}'),
+                ),
+                separatorBuilder: (context, index) => const Divider(
+                  height: 1,
+                  thickness: 1,
+                  color: Colors.grey,
+                ),
+                itemCount: logic.postForLists.length,
+              ),
       ),
     );
   }
