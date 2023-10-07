f27cdbc15e5d0b2dbd819828a1212604ff3bfc1a
SaltPepperSalt <dksro324@gmail.com>
2023-10-07 13:18:46 +0900

    FEAT: refactoring code

--- lib/app/login/view.dart
+++ lib/app/login/view.dart
@@ -26,65 +26,101 @@ class LoginPage extends StatelessWidget {
                       : const Text('Type Email To login'),
                 ),
                 Obx(
-                  () => TextField(
-                    controller: logic.emailController,
-                    decoration: InputDecoration(
-                      errorText: logic.errorMessage.value == ''
-                          ? null
-                          : logic.errorMessage.value,
-                    ),
-                    onChanged: (String? value) {
-                      logic.errorMessage.value = '';
-                    },
-                  ),
+                  () => LoginTextField(),
                 ),
                 const SizedBox(
                   height: 40.0,
                 ),
                 Obx(
-                  () => logic.isError.value
-                      ? TextButton(
-                          onPressed: () {
-                            logic.fetchUserList();
-                          },
-                          style: TextButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            fixedSize: const Size(160, 20),
-                          ),
-                          child: logic.isLoadingUserList.value
-                              ? const SizedBox(
-                                  width: 20.0,
-                                  height: 20.0,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2.0,
-                                  ),
-                                )
-                              : const Text('Tap To Refresh'),
-                        )
-                      : TextButton(
-                          onPressed: () {
-                            logic.authService.processLogin(logic.login);
-                          },
-                          style: TextButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            fixedSize: const Size(80, 20),
-                          ),
-                          child: logic.isLoadingUserList.value
-                              ? const SizedBox(
-                                  width: 20.0,
-                                  height: 20.0,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2.0,
-                                  ),
-                                )
-                              : const Text('Login'),
-                        ),
+                  () => logic.isError.value ? RetryButton() : LoginButton(),
                 ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class LoginTextField extends StatelessWidget {
+  LoginTextField({
+    super.key,
+  });
+
+  final LoginPageLogic logic = Get.find<LoginPageLogic>();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: logic.emailController,
+      decoration: InputDecoration(
+        errorText:
+            logic.errorMessage.value == '' ? null : logic.errorMessage.value,
+      ),
+      onChanged: (String? value) {
+        logic.errorMessage.value = '';
+      },
+    );
+  }
+}
+
+class RetryButton extends StatelessWidget {
+  RetryButton({
+    super.key,
+  });
+
+  final LoginPageLogic logic = Get.find<LoginPageLogic>();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        logic.fetchUserList();
+      },
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.black,
+        fixedSize: const Size(160, 20),
+      ),
+      child: logic.isLoadingUserList.value
+          ? const SizedBox(
+              width: 20.0,
+              height: 20.0,
+              child: CircularProgressIndicator(
+                strokeWidth: 2.0,
+              ),
+            )
+          : const Text('Tap To Refresh'),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  LoginButton({
+    super.key,
+  });
+
+  final LoginPageLogic logic = Get.find<LoginPageLogic>();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        logic.authService.processLogin(logic.login);
+      },
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.black,
+        fixedSize: const Size(80, 20),
+      ),
+      child: logic.isLoadingUserList.value
+          ? const SizedBox(
+              width: 20.0,
+              height: 20.0,
+              child: CircularProgressIndicator(
+                strokeWidth: 2.0,
+              ),
+            )
+          : const Text('Login'),
     );
   }
 }
